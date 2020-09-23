22353b512c9446e7379fdb0a34d5f839082a8279
Formatter <KomodoKode@users.noreply.github.com>
2020-09-23 18:55:01 +0000

    :robot: | :art: - :black_circle: -> Blackened files! [skip ci]

--- Primes.py
+++ Primes.py
@@ -12,26 +12,90 @@ rng = secrets.SystemRandom()
 """Gets the Random Number Generator
 to generate probabilistic test numbers"""
 
-base_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-               31, 37, 41, 43, 47, 53, 59, 61, 67,
-               71, 73, 79, 83, 89, 97, 101, 103,
-               107, 109, 113, 127, 131, 137, 139,
-               149, 151, 157, 163, 167, 173, 179,
-               181, 191, 193, 197, 199, 211, 223,
-               227, 229, 233, 239, 241, 251, 257,
-               263, 269, 271, 277, 281, 283, 293,
-               307, 311, 313, 317, 331, 337, 347, 349]
+base_primes = [
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+    103,
+    107,
+    109,
+    113,
+    127,
+    131,
+    137,
+    139,
+    149,
+    151,
+    157,
+    163,
+    167,
+    173,
+    179,
+    181,
+    191,
+    193,
+    197,
+    199,
+    211,
+    223,
+    227,
+    229,
+    233,
+    239,
+    241,
+    251,
+    257,
+    263,
+    269,
+    271,
+    277,
+    281,
+    283,
+    293,
+    307,
+    311,
+    313,
+    317,
+    331,
+    337,
+    347,
+    349,
+]
+
 
 def bitrandom(n):
     return secrets.randbits(n - 1)
 
 
-#Test to see if p1 & p2 are prime using Fermat's Theorem
-def Fermat_prime(integer : int) -> bool:
+# Test to see if p1 & p2 are prime using Fermat's Theorem
+def Fermat_prime(integer: int) -> bool:
     numTrials = 1001
     testnums = set()
     valid = 0
-    while valid < numTrials :
+    while valid < numTrials:
         rand_testnum = rng.randint(2, integer - 1)
         if rand_testnum not in testnums:
             if gcd(integer, rand_testnum) != 1:
@@ -43,8 +107,8 @@ def Fermat_prime(integer : int) -> bool:
     return True
 
 
-#Test to see if p1 & p2 are prime using Rabin-Miller Prime Test
-def RabinMiller_prime(n : int) -> bool :
+# Test to see if p1 & p2 are prime using Rabin-Miller Prime Test
+def RabinMiller_prime(n: int) -> bool:
     if n & 1 == 0:
         return False
     s = n - 1
@@ -68,8 +132,9 @@ def RabinMiller_prime(n : int) -> bool :
         else:
             return False
     return True
-    
-def GetBasicPrime(bits : int) -> int:
+
+
+def GetBasicPrime(bits: int) -> int:
     testedset = set()
     while True:
         testnum = bitrandom(bits)
@@ -79,14 +144,15 @@ def GetBasicPrime(bits : int) -> int:
                     testedset.add(testnum)
             return testnum
 
-def GetRandPrime(numbits : int) -> int:
-    '''Not 100% guaranteed to work,
-    but highly likely to give true prime'''
+
+def GetRandPrime(numbits: int) -> int:
+    """Not 100% guaranteed to work,
+    but highly likely to give true prime"""
     trialset = set()
     while True:
         trialnum = GetBasicPrime(numbits)
         if trialnum not in trialset:
-            #Prime tests number using Fermat's Primality Test
+            # Prime tests number using Fermat's Primality Test
             if RabinMiller_prime(trialnum) == True:
                 return trialnum
             else:
--- main.py
+++ main.py
@@ -15,23 +15,24 @@ from Primes import GetRandPrime
 
 
 class KeyGenerator:
-  
-  def __init__(self): 
-    self.rng = secrets.SystemRandom()
-  
-  def generate_RSAkeys(bits, pub_e=65537):
-    self.pub_e = pub_e
-    if bits > 1024:
-      raise NotImplementedError("Prime Numbers cannot currently be over 1024 Bits")
-    self.p1 = GetRandPrime(bits)
-    self.p2 = GetRandPrime(bits)
-    self.pub_key = p1 * p2
-    # Key Creation
-    self.phi_pubkey = (p1 - 1) * (p2 - 1)
+    def __init__(self):
+        self.rng = secrets.SystemRandom()
 
-    self.priv_key = pow(self.pub_e, -1, self.phi_pubkey)
+    def generate_RSAkeys(bits, pub_e=65537):
+        self.pub_e = pub_e
+        if bits > 1024:
+            raise NotImplementedError(
+                "Prime Numbers cannot currently be over 1024 Bits"
+            )
+        self.p1 = GetRandPrime(bits)
+        self.p2 = GetRandPrime(bits)
+        self.pub_key = p1 * p2
+        # Key Creation
+        self.phi_pubkey = (p1 - 1) * (p2 - 1)
 
-    """
+        self.priv_key = pow(self.pub_e, -1, self.phi_pubkey)
+
+        """
     Calculates the modular
     inverse of e and phi_n.
     This feature is only in
@@ -41,38 +42,38 @@ class KeyGenerator:
     repository soon.
     """
 
-    # Map of keys to not confuse keys
-    self.key_map =  {"public key" : self.pub_key,"pub_e" : self.pub_e, "private key": self.priv_key}
-    return self.key_map
+        # Map of keys to not confuse keys
+        self.key_map = {
+            "public key": self.pub_key,
+            "pub_e": self.pub_e,
+            "private key": self.priv_key,
+        }
+        return self.key_map
 
-  def Derive_SecretKey(self, encrypted_skey):
-    """This function derives a key from a encrypted key given
-    by using the Encrypt_SecretKey() method."""
+    def Derive_SecretKey(self, encrypted_skey):
+        """This function derives a key from a encrypted key given
+        by using the Encrypt_SecretKey() method."""
 
-    SecretKey = pow(encrypted_skey, self.priv_key, self.pub_key)
-    self.skey = SecretKey
-    keymap.update({"secret key" : self.skey})
-    return SecretKey
-    
-  def GetHexValues(self, values="all"):
-    """Gets the hexidecimal values of the keys
-    default value is all values, but can be changed
-    to the key values."""
-    if values == "all":
-      hex_keymap = {}
-      for i in self.keymap:
-        hex_keymap.update({i, self.keymap[i]})
-      self.hexkeys = hex_keymap
-      return hex_keymap
-      
-  def Encrypt_SecretKey(self, bits):
-    """Generates a secret key for using in exchange.
-    Since this value is special and isn't saved as a
-    class variable, it automatically presents the hex
-    value of the key."""
-    secretkey = self.rng.getrandbits(bits)
-    return secretkey, hex(secretkey)
+        SecretKey = pow(encrypted_skey, self.priv_key, self.pub_key)
+        self.skey = SecretKey
+        keymap.update({"secret key": self.skey})
+        return SecretKey
 
+    def GetHexValues(self, values="all"):
+        """Gets the hexidecimal values of the keys
+        default value is all values, but can be changed
+        to the key values."""
+        if values == "all":
+            hex_keymap = {}
+            for i in self.keymap:
+                hex_keymap.update({i, self.keymap[i]})
+            self.hexkeys = hex_keymap
+            return hex_keymap
 
-
-
+    def Encrypt_SecretKey(self, bits):
+        """Generates a secret key for using in exchange.
+        Since this value is special and isn't saved as a
+        class variable, it automatically presents the hex
+        value of the key."""
+        secretkey = self.rng.getrandbits(bits)
+        return secretkey, hex(secretkey)
